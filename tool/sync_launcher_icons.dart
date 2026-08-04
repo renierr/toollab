ba60429e737462fd38c105aea2ad25cc53f2af6c
Renier <renierr@users.noreply.github.com>
2026-08-04 18:27:26 +0200

    Use white tool icon backgrounds

--- tool/sync_launcher_icons.dart
+++ tool/sync_launcher_icons.dart
@@ -94,8 +94,12 @@ void main() async {
     );
     await adaptiveIcon.writeAsString('''<?xml version="1.0" encoding="utf-8"?>
 <adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
-  <background android:drawable="@color/ic_launcher_background" />
-  <foreground android:drawable="@drawable/${resourceName}_foreground" />
+  <background android:drawable="@color/ic_tool_launcher_background" />
+  <foreground>
+    <inset
+        android:drawable="@drawable/${resourceName}_foreground"
+        android:inset="16%" />
+  </foreground>
 </adaptive-icon>
 ''');
     synced++;
